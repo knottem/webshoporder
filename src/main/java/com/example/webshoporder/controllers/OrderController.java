ecbf08c31467065824012e62b731f796269e98ad
knottem <59744487+knottem@users.noreply.github.com>
2023-05-17 16:33:42 +0200

    updated service and removed from controller

--- src/main/java/com/example/webshoporder/controllers/OrderController.java
+++ src/main/java/com/example/webshoporder/controllers/OrderController.java
@@ -36,33 +36,4 @@ public class OrderController {
     public ResponseEntity<Object> buyItem(@RequestBody BuyOrder buyOrder) {
         return orderService.buyItem(buyOrder);
     }
-
-
-    /*
-    @PostMapping("/orders/buy")
-    public ResponseEntity<Object> buyItem(@RequestBody BuyOrder buyOrder) {
-        //get from customer api
-        Customer customer = customerRepository.findById(buyOrder.getCustomerId()).isPresent() ? customerRepository.findById(buyOrder.getCustomerId()).get() : null;
-        if (customer == null) {
-            return new ResponseEntity<>(Collections.singletonMap("error", "Customer not found"), HttpStatus.NOT_FOUND);
-        } else {
-            List <Item> itemList = new ArrayList<>();
-            List<Long> items = buyOrder.getItemIds();
-            for (Long item : items) {
-                //Item itemFromDb = get from item api
-                Item itemFromDb = itemRepository.findById(item).isPresent() ? itemRepository.findById(item).get() : null;
-                if (itemFromDb == null) {
-                    return new ResponseEntity<>(Collections.singletonMap("error", "Product not found"), HttpStatus.NOT_FOUND);
-                } else {
-                    itemList.add(itemFromDb);
-                }
-            }
-            Order order = new Order(customer, itemList);
-            orderRepository.save(order);
-            logger.info("Order created: " + order);
-            return new ResponseEntity<>(order, HttpStatus.CREATED);
-        }
-    }
-
- */
 }
